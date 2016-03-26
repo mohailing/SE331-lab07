6a800e3e057e8f94e1fcffbdeaee9c3be6f3f41f
mohailing <18295671930@163.com>
2016-03-26 17:22:53 +0800

    second

--- src/main/java/camt/se331/shoppingcart/dao/DbProductDao.java
+++ src/main/java/camt/se331/shoppingcart/dao/DbProductDao.java
@@ -3,5 +3,5 @@ package camt.se331.shoppingcart.dao;
 /**
  * Created by mohailing on 3/26/2016.
  */
-public interface ProductDao {
+public class DbProductDao {
 }
---
+++ src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package camt.se331.shoppingcart.repository;
+
+/**
+ * Created by mohailing on 3/26/2016.
+ */
+public interface ProductRepository {
+}
